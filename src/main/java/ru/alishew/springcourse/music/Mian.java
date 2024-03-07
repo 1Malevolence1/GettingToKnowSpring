82b98808c17b3d7c2ff96701276c2e39646a5e75
Mr.D <danil36rusdanil@mail.ru>
2024-03-07 17:34:11 +0300

    Научился передавать значения в объекты через сетре

--- src/main/java/ru/alishew/springcourse/music/Mian.java
+++ src/main/java/ru/alishew/springcourse/music/Mian.java
@@ -5,11 +5,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Mian {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        Player player = new Player(context.getBean("music", Music.class));
+        Player player = context.getBean("musicPlayer", Player.class);
 
         player.play();
 
 
+        System.out.println(player.getName());
+        System.out.println(player.getValue());
         context.close();
     }
 }
--- src/main/java/ru/alishew/springcourse/music/Player.java
+++ src/main/java/ru/alishew/springcourse/music/Player.java
@@ -3,6 +3,33 @@ package ru.alishew.springcourse.music;
 public class Player {
     private Music music;
 
+    private String name;
+
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public Player() {
+    }
+
     // внедряется зависимость из вне (IoC)
     public Player(Music music) {
         this.music = music;
